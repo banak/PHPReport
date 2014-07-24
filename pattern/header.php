14227916b1174fb78151d217365daceb80bfa46a
banak <sebmiehe@iMac-de-Sebastien.local>
2014-07-24 23:31:22 +0200

    1st insertion des gridster

--- pattern/header.php
+++ pattern/header.php
@@ -5,9 +5,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 echo '<head>';
 echo '<meta http-equiv="content-type" content="text/html; charset=utf-8" />';
 echo '<link rel="stylesheet" href="./css/styleObject.css" type="text/css" media="screen" />';
 echo '<script src="./lib/jquery-1.11.1.min.js"></script>';
+echo '<link rel="stylesheet" type="text/css" href="./css/styles.css">';
+echo '<script src="./lib/ducksboard-gridster.js-7640971/dist/jquery.gridster.min.js" type="text/javascript" charset="utf-8"></script>';
+echo '<link rel="stylesheet" type="text/css" href="./lib/ducksboard-gridster.js-7640971/dist/jquery.gridster.css">';
+echo '<script src="./lib/ducksboard-gridster.js-7640971/src/jquery.gridster.js"></script>';
+
 echo '</head>';
 
+?>
